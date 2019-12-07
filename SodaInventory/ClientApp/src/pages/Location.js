faaa632d72167c7225ad98b01f813b0b0bce3211
Cristian <cristianlanderos2@gmail.com>
2019-12-06 20:35:16 -0700

    Modified ui

--- SodaInventory/ClientApp/src/pages/Location.js
+++ SodaInventory/ClientApp/src/pages/Location.js
@@ -3,7 +3,7 @@ import {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import * as LocationStore from '../store/Location';
-import {Button, Header, Icon, Segment} from 'semantic-ui-react';
+import {Button, Card, Container, Header, Icon, Segment} from 'semantic-ui-react';
 import AddLocation from "../modals/AddLocationModal";
 
 class Counter extends Component {
@@ -16,30 +16,43 @@ class Counter extends Component {
     }
 
     closeModal = () => {
-      this.setState({showAddLocationModal: false})
+        this.setState({showAddLocationModal: false})
     };
 
     render() {
         return (
             <div>
                 <AddLocation showModal={this.state.showAddLocationModal} closeModal={this.closeModal}/>
-                <Header as='h2' attached='top'>Locations</Header>
-                <Segment attached>
-                    <Header as='h3'>
-                        <Icon name='building' circular/>
-                        <Header.Content>Add a location</Header.Content>
+                <Container>
+                    <Header as='h2' attached='top'>
+                        Select Store
+                        <Button floated='right' primary onClick={() => this.setState({showAddLocationModal: true})}>Add Store</Button>
                     </Header>
+                    <Segment attached>
+                        <Header as='h3' className='mb-4'>
+                            <Icon name='building' circular/>
+                            <Header.Content>Add a store</Header.Content>
+                        </Header>
 
-                    {this.props.locations.map((location) =>
-                        <Link to='/'>
-                            <h3 key={location.id}>{location.name}</h3>
-                            <div className="ui section divider"/>
-                        </Link>
-                    )}
+                        <Container>
+                            <Card fluid onClick={() => console.log("Click on item")}>
+                                <Card.Content>
+                                    <Icon name="edit" style={{float:'right'}} size={"big"} className="mt-1"/>
+                                    <Card.Header>Store 1</Card.Header>
+                                    <Card.Meta>1234 E. 567 S. Provo UT</Card.Meta>
+                                </Card.Content>
+                            </Card>
 
-                    <Button primary onClick={() => this.setState({showAddLocationModal: true})}>Add Store</Button>
-                </Segment>
+                        {this.props.locations.map((location) =>
+                            <Link to='/'>
+                                <h3 key={location.id}>{location.name}</h3>
+                                <div className="ui section divider"/>
+                            </Link>
+                        )}
 
+                        </Container>
+                    </Segment>
+                </Container>
             </div>
         );
     }
